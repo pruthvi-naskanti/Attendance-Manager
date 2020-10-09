257e9bab20ba657821b127915d175d5668b1762b
Yashwanth <yashwanthdamera6@gmail.com>
2020-10-09 14:15:56 -0500

    Added String Arrays and used this in spinner to get drop down

--- app/src/main/java/com/example/attendancemanager/MainActivity.java
+++ app/src/main/java/com/example/attendancemanager/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.attendancemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    Button login;
+    Spinner spinner;
+    String userrole;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login=findViewById(R.id.loginBtn);
+        spinner=findViewById(R.id.spinnerloginas);
+        spinner.setOnItemSelectedListener(this);
+
+
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this,adapterView.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
