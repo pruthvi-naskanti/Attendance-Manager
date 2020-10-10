7a2d4977fd01d0f91dcf02715a064de069ddf6ef
pruthvi-naskanti <pruthvinaskanti@gmail.com>
2020-10-09 19:36:28 -0500

    Pruthvi - Created Java class for submit button in View student screen

--- app/src/main/java/com/example/attendancemanager/ViewStudentActivity.java
+++ app/src/main/java/com/example/attendancemanager/ViewStudentActivity.java
@@ -1,6 +1,9 @@
 package com.example.attendancemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +14,13 @@ public class ViewStudentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_student);
 
+    }
+
+    public void submitbtn(View v) {
+        Intent I = new Intent(getApplicationContext(), ViewAttendanceList.class);
+        startActivity(I);
+        setContentView(R.layout.view_attendence_list);
+        Toast.makeText(getApplicationContext(), "List of students in Class", Toast.LENGTH_LONG).show();
     }
 
 }
