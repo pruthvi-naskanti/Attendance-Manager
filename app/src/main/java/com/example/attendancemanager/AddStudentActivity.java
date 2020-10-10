a34874efcc11e6aafebfc973aa0f8d005238a04e
pruthvi-naskanti <pruthvinaskanti@gmail.com>
2020-10-09 19:06:26 -0500

    Pruthvi - created onClick method for SUBMIT button in Add student screen

--- app/src/main/java/com/example/attendancemanager/AddStudentActivity.java
+++ app/src/main/java/com/example/attendancemanager/AddStudentActivity.java
@@ -1,6 +1,9 @@
 package com.example.attendancemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,12 @@ public class AddStudentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_student);
 
+    }
+
+    public void submit(View v) {
+        Intent I = new Intent(getApplicationContext(), HomeScreenActivity.class);
+        startActivity(I);
+        setContentView(R.layout.home_screen);
+        Toast.makeText(getApplicationContext(), "Student Successfully Added", Toast.LENGTH_LONG).show();
     }
 }
