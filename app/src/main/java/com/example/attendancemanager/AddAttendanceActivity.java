87b7b4fcdf1cf4e6c2d35b9723ed3ff20c3e2eac
vishal Reddy <vishalreddy0507@gmail.com>
2020-10-09 20:41:50 -0500

    modified addstudentActivity- Vishal Reddy Vennavaram

--- app/src/main/java/com/example/attendancemanager/AddAttendanceActivity.java
+++ app/src/main/java/com/example/attendancemanager/AddAttendanceActivity.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RadioButton;
@@ -11,19 +12,30 @@ import android.widget.RadioGroup;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View.OnClickListener;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AddAttendanceActivity extends AppCompatActivity {
     private ListView listView;
     String status;
     Button attendanceSubmit;
+    String[] studentslist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listview_main);
-        listView = findViewById(R.id.listview);
+
+
     }
 
     public void listview(View v) {
+        listView = findViewById(R.id.listview);
+        studentslist=new String[]{
+                "Student ABC",
+                "Student XYZ"
+        };
         final Dialog dialog = new Dialog(AddAttendanceActivity.this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.test_layout);
@@ -33,6 +45,10 @@ public class AddAttendanceActivity extends AppCompatActivity {
         radioGroup = (RadioGroup) dialog.findViewById(R.id.radioGroup);
         present = (RadioButton) dialog.findViewById(R.id.PresentradioButton);
         absent = (RadioButton) dialog.findViewById(R.id.AbsentradioButton);
+        final List<String> student_list = new ArrayList<String>(Arrays.asList(studentslist));
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, student_list);
+        listView.setAdapter(arrayAdapter);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
