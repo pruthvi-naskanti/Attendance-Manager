349d4ea2f6958178e0c192de2d60e5b4c16fda03
Yashwanth <yashwanthdamera6@gmail.com>
2020-10-30 17:38:37 -0500

    Updated ViewStudentListActivity.java with static data- Venkatayashwanth

--- app/src/main/java/com/example/attendancemanager/AttendancePerStudent.java
+++ app/src/main/java/com/example/attendancemanager/AttendancePerStudent.java
@@ -1,17 +1,27 @@
 package com.example.attendancemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AttendancePerStudent extends AppCompatActivity {
 
+    String[] data={"Student 1- present ","Student 2- absent","Student 3- present"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listview_main);
 
+        ListView lv=(ListView) findViewById(R.id.listview);
 
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+
+        lv.setAdapter(arrayAdapter);
 
     }
 }
--- app/src/main/java/com/example/attendancemanager/ViewStudentListActivity.java
+++ app/src/main/java/com/example/attendancemanager/ViewStudentListActivity.java
@@ -1,15 +1,23 @@
 package com.example.attendancemanager;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ViewStudentListActivity extends AppCompatActivity {
+    String[] data={"Id No 101: Student 1","Id No 102: Student 2","Id No 103: Student 3"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listview_main);
 
+        ListView lv=(ListView) findViewById(R.id.listview);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+
+        lv.setAdapter(arrayAdapter);
     }
 
 
