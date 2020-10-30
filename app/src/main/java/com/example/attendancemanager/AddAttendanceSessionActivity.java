d7263c2e5a0ce382dd392aa709ebcdbad948d19d
vishal Reddy <vishalreddy0507@gmail.com>
2020-10-30 15:47:34 -0500

    created instance for various fields to add calendar in the AddAttendanceSessionActivity.java - Vishal Reddy Vennavaram

--- app/src/main/java/com/example/attendancemanager/AddAttendanceSessionActivity.java
+++ app/src/main/java/com/example/attendancemanager/AddAttendanceSessionActivity.java
@@ -5,20 +5,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class AddAttendanceSessionActivity extends AppCompatActivity {
     Spinner dept;
     Spinner year;
     Button submit;
+    private EditText dateEditText;
+    private ImageButton date;
+    private Calendar cal;
+    private int day;
+    private int month;
+    private int dyear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_attendance);
         submit=findViewById(R.id.buttonsubmit);
+        date = (ImageButton) findViewById(R.id.DateImageButton);
+        cal = Calendar.getInstance();
+        day = cal.get(Calendar.DAY_OF_MONTH);
+        month = cal.get(Calendar.MONTH);
+        dyear = cal.get(Calendar.YEAR);
+        dateEditText = (EditText) findViewById(R.id.DateEditText);
     }
 
     public void view_attendance(View v){
