9aa9252ace74ca902a86d894d6b76b54db097b0f
Bhaskar <reddybhaskar437@gmail.com>
2020-10-30 15:20:40 -0500

    created instance for AddFaculty.java in add add faculty activity. and created instance for firebase authentication- Bhaskar reddy

--- app/src/main/java/com/example/attendancemanager/AddFaculty.java
+++ app/src/main/java/com/example/attendancemanager/AddFaculty.java
@@ -4,19 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AddFaculty extends AppCompatActivity {
 
     Button B;
+    FirebaseAuth fAuth;
+    EditText firstname;
+    EditText lastname;
+    EditText Mobilenumber;
+    EditText Address;
+    EditText UserName;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_faculty);
         B=findViewById(R.id.RegisterButton);
+        fAuth = FirebaseAuth.getInstance();
+
+        firstname=findViewById(R.id.editTextFirstName);
+        lastname=findViewById(R.id.editTextLastName);
+        Mobilenumber=findViewById(R.id.editTextPhone);
+        Address=findViewById(R.id.editTextaddr);
+        UserName=findViewById(R.id.editTextUserName);
+        password=findViewById(R.id.editTextPassword);
         B.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
