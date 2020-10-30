a6bf88aa0d922efe42495708ffbabf753c8d67f9
pruthvi-naskanti <pruthvinaskanti@gmail.com>
2020-10-30 17:52:15 -0500

    updated ViewAttendanceByFacultyActivity.java with static data- pruthvi

--- app/src/main/java/com/example/attendancemanager/ViewAttendanceByFacultyActivity.java
+++ app/src/main/java/com/example/attendancemanager/ViewAttendanceByFacultyActivity.java
@@ -2,10 +2,20 @@ package com.example.attendancemanager;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ViewAttendanceByFacultyActivity extends Activity {
+    String[] data={"Id No 101: Student 1: Absent ","Id No 102: Student 2: Present","Id No 103: Student 3: Absent"};
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listview_main);
+
+        ListView lv=(ListView) findViewById(R.id.listview);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+
+        lv.setAdapter(arrayAdapter);
     }
 }
