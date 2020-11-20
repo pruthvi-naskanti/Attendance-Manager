65cbe6454d692ca499a5fa136c567f8208dfdd6f
Bhaskar <reddybhaskar437@gmail.com>
2020-11-20 15:01:08 -0600

    added view_todays_attendance.xml which will display today attendance of the student on the selected date and I have created the facultyadapter.java

---
+++ app/src/main/java/com/example/attendancemanager/adapter/FacultyAdapter.java
@@ -0,0 +1,53 @@
+package com.example.attendancemanager.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.attendancemanager.R;
+import com.example.attendancemanager.models.Faculty;
+
+import java.util.List;
+
+public class FacultyAdapter extends RecyclerView.Adapter<FacultyAdapter.FacultyViewHolder> {
+
+    private final List<Faculty> data;
+
+    public FacultyAdapter(List<Faculty> data) {
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public FacultyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem = layoutInflater.inflate(R.layout.item_faculty, parent, false);
+        return new FacultyViewHolder(listItem);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FacultyViewHolder holder, int position) {
+       Faculty item = data.get(position);
+       holder.name.setText(item.getFirstName() + " " +item.getLastName() );
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    static class FacultyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView name;
+
+        public FacultyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.tvItemFaculty);
+        }
+    }
+
+}
