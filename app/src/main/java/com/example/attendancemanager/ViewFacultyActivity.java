ea553941356349443670288eb1983123aa6ce5c5
vishal Reddy <vishalreddy0507@gmail.com>
2020-11-20 14:40:44 -0600

    defined backend logic and functionality for ViewFacultyActivity.java and ViewStudentActivity.java files to list students and faculty - pruthvi

--- app/src/main/java/com/example/attendancemanager/ViewFacultyActivity.java
+++ app/src/main/java/com/example/attendancemanager/ViewFacultyActivity.java
@@ -1,18 +1,64 @@
 package com.example.attendancemanager;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.attendancemanager.adapter.FacultyAdapter;
+import com.example.attendancemanager.models.Faculty;
+import com.example.attendancemanager.other.Constants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewFacultyActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+    private RecyclerView recyclerView;
+    private FacultyAdapter adapter;
+    private final List<Faculty> facultyList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_faculty_list);
 
+        db = FirebaseFirestore.getInstance();
+        recyclerView =findViewById(R.id.rvViewFaculty);
+
+        getFacultyList();
+
     }
 
+    private void getFacultyList() {
+
+        db.collection(Constants.faculty)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for (QueryDocumentSnapshot item : task.getResult()){
+                                facultyList.add((item.toObject(Faculty.class)));
+                            }
+                            adapter = new FacultyAdapter(facultyList);
+                            recyclerView.setAdapter(adapter);
+                        }else{
+                            Toast.makeText(ViewFacultyActivity.this, getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
 
 
 }
--- app/src/main/java/com/example/attendancemanager/ViewStudentActivity.java
+++ app/src/main/java/com/example/attendancemanager/ViewStudentActivity.java
@@ -4,29 +4,73 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.attendancemanager.models.Student;
+import com.example.attendancemanager.other.Constants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewStudentActivity extends AppCompatActivity {
 
-    Button B;
+    private Button B;
+    private FirebaseFirestore db;
+    private ArrayList<Student> studentList = new ArrayList<>();
+    private Spinner spinnerDept;
+    private Spinner spinnerYear;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_student);
+
+        db = FirebaseFirestore.getInstance();
+
+        spinnerDept = findViewById(R.id.spinnerbranchView);
+        spinnerYear = findViewById(R.id.spinneryearView);
+
         B=findViewById(R.id.submitButton);
         B.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent I = new Intent(getApplicationContext(), ViewStudentListActivity.class);
-                startActivity(I);
-                //setContentView(R.layout.listview_main);
+                getFilteredStudent(spinnerDept.getSelectedItem().toString(),spinnerYear.getSelectedItem().toString());
             }
         });
-
     }
 
+    private void getFilteredStudent(String dept,String year){
+
+        db.collection(Constants.student)
+                .whereEqualTo(Constants.dept,dept)
+                .whereEqualTo(Constants.year,year)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for(QueryDocumentSnapshot item : task.getResult()){
+                                studentList.add(item.toObject(Student.class));
+                            }
+                            Intent i = new Intent(getApplicationContext(), ViewStudentListActivity.class);
+                            i.putParcelableArrayListExtra(Constants.STUDENT_LIST,studentList);
+                            startActivity(i);
+                        }else{
+                            Toast.makeText(ViewStudentActivity.this, getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
 
 
+    }
 }
