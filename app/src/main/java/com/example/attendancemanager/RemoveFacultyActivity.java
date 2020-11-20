5a3608e9a6982b337a21e2fad68c4749c3b00748
Yashwanth <yashwanthdamera6@gmail.com>
2020-11-20 13:55:10 -0600

    RemoveFacultyActivity.java activity is used to remove the faculty with user name. RemoveStudentActivity.java activity is used to remove the student by using their id which is autogenerated by student - yashwanth

--- app/src/main/java/com/example/attendancemanager/RemoveFacultyActivity.java
+++ app/src/main/java/com/example/attendancemanager/RemoveFacultyActivity.java
@@ -1,40 +1,102 @@
 package com.example.attendancemanager;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.attendancemanager.other.Constants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class RemoveFacultyActivity extends AppCompatActivity {
-    Button Remove;
-    Button Cancel;
+
+    private FirebaseFirestore db;
+    private EditText editText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.remove_faculty);
-        Remove=findViewById(R.id.facremovebtn);
-        Cancel=findViewById(R.id.faccbtn);
-        Remove.setOnClickListener(new View.OnClickListener() {
+
+        db = FirebaseFirestore.getInstance();
+        editText = findViewById(R.id.facfnameET);
+        Button remove = findViewById(R.id.facremovebtn);
+        Button cancel = findViewById(R.id.faccbtn);
+
+        remove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent I=new Intent(getApplicationContext(), HomeScreenActivity.class);
-                startActivity(I);
-                //setContentView(R.layout.home_screen);
-                Toast.makeText(getApplicationContext(), "Removed Faculty", Toast.LENGTH_LONG).show();
+                String username = editText.getText().toString();
+                if (!validateInput(username)) return;
+                checkFacultyExist(username);
             }
         });
 
-        Cancel.setOnClickListener(new View.OnClickListener() {
+        cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent I=new Intent(getApplicationContext(), HomeScreenActivity.class);
-                startActivity(I);
-                //setContentView(R.layout.home_screen);
+                finish();
+            }
+        });
+    }
+
+    private boolean validateInput(String username) {
+        if (username.isEmpty()) {
+            Toast.makeText(this, getString(R.string.username_cannot_empty), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void checkFacultyExist(String username) {
+
+        db.collection(Constants.faculty)
+                .whereEqualTo(Constants.username, username)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (task.getResult().size() == 1) {
+                                deleteFaculty(task.getResult().getDocuments().get(0).getId());
+                                return;
+                            }else{
+                                Toast.makeText(RemoveFacultyActivity.this,"Username not exists", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        Toast.makeText(RemoveFacultyActivity.this, getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void deleteFaculty(String documentId) {
+
+        db.collection(Constants.faculty).document(documentId)
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(RemoveFacultyActivity.this, getString(R.string.faculty_deleted), Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RemoveFacultyActivity.this, getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
             }
         });
 
     }
+
+
 }
--- app/src/main/java/com/example/attendancemanager/RemoveStudentActivity.java
+++ app/src/main/java/com/example/attendancemanager/RemoveStudentActivity.java
@@ -1,48 +1,101 @@
 package com.example.attendancemanager;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.attendancemanager.other.Constants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class RemoveStudentActivity extends AppCompatActivity {
 
-    Button REMOVE;
-    Button CANCEL;
+    private Button REMOVE;
+    private Button CANCEL;
+    private EditText etStudentID;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        db = FirebaseFirestore.getInstance();
+
         setContentView(R.layout.remove_student);
         REMOVE=findViewById(R.id.removebtn);
         CANCEL=findViewById(R.id.cbtn);
+        etStudentID = findViewById(R.id.fnameET);
 
         REMOVE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent I=new Intent(getApplicationContext(), HomeScreenActivity.class);
-                startActivity(I);
-               // setContentView(R.layout.home_screen);
-                Toast.makeText(getApplicationContext(), "Removed Student", Toast.LENGTH_LONG).show();
+                String studentID = etStudentID.getText().toString();
+                if (!validateInput(studentID)) return;
+                fetchStudentDocumentID(studentID);
             }
         });
 
         CANCEL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent I=new Intent(getApplicationContext(), HomeScreenActivity.class);
-                startActivity(I);
-                //setContentView(R.layout.home_screen);
+               finish();
             }
         });
-
-
-
     }
 
+    private void fetchStudentDocumentID(String studentId){
+        db.collection(Constants.student)
+                .whereEqualTo(Constants.id,Integer.parseInt(studentId))
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            if (task.getResult().size()==1){
+                                deleteStudentDocument(task.getResult().getDocuments().get(0).getId());
+                            }else {
+                                Toast.makeText(RemoveStudentActivity.this, getString(R.string.student_not_exist), Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                            Toast.makeText(RemoveStudentActivity.this,getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    private void deleteStudentDocument(String documentID){
+        db.collection(Constants.student).document(documentID).delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(RemoveStudentActivity.this, getString(R.string.student_deleted), Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RemoveStudentActivity.this,getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private boolean validateInput(String id) {
+        if (id.isEmpty()){
+            Toast.makeText(this, getString(R.string.student_id_empty), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
 
 
 }
