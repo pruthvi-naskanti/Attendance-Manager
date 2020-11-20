9b9cc5e8c1402455888698ad2291d516e8ce3db6
vishal Reddy <vishalreddy0507@gmail.com>
2020-11-20 15:23:33 -0600

    updated AndroidManifest.xml with the user permissions to access internet and also added Constants.java with all the constant string values that are used by all the activities

---
+++ app/src/main/java/com/example/attendancemanager/other/Constants.java
@@ -0,0 +1,32 @@
+package com.example.attendancemanager.other;
+
+public class Constants {
+
+    public static final String loginType = "loginType";
+    public static final String ADMIN = "admin";
+    public static final String FACULTY = "faculty";
+    public static final String FACULTY_LIST = "faculty_list";
+    public static final String STUDENT_LIST = "student_list";
+    public static final String SUBJECT = "subject";
+    public static final String DATE = "date";
+    public static final String AVAILABILITY = "availability";
+    public static final String DATE_FORMAT = "dd-MMM-YY";
+
+    public static final String id = "id";
+    public static final String dept = "dept";
+    public static final String year = "year";
+    public static final String username = "username";
+    public static final String faculty = "faculty";
+    public static final String student = "student";
+    public static final String recentStudentIndex = "recentStudentIndex";
+    public static final String recentStudentId = "recentStudentId";
+    public static final String javaAttendance = "javaAttendance";
+    public static final String androidAttendance = "androidAttendance";
+    public static final String iOSAttendance = "iOSAttendance";
+    public static final String mlAttendance = "mlAttendance";
+    public static final String adbAttendance = "adbAttendance";
+
+
+
+
+}
