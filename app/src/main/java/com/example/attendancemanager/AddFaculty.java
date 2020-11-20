6d36076e5232be24eceed039948b08edcf852ad6
Yashwanth <yashwanthdamera6@gmail.com>
2020-11-20 13:33:08 -0600

    AddFaculty.java used to add the faculty to the application with user name and password. faculty collection database is used to create the faculty with unique username. AddStudentActivity.java is updated, to add the student with the first name and last name and id of the student will be autogenerated to the student in the student db collection

--- app/src/main/java/com/example/attendancemanager/AddFaculty.java
+++ app/src/main/java/com/example/attendancemanager/AddFaculty.java
@@ -2,73 +2,41 @@ package com.example.attendancemanager;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.attendancemanager.models.Faculty;
+import com.example.attendancemanager.other.Constants;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class AddFaculty extends AppCompatActivity {
 
-    Button B;
-    FirebaseAuth fAuth;
-    EditText firstname;
-    EditText lastname;
-    EditText Mobilenumber;
-    EditText Address;
-    EditText UserName;
-    EditText password;
+    private EditText etFirstName, etLastName, etUsername, etPassword, etAddress, etPhone;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_faculty);
-        B=findViewById(R.id.RegisterButton);
-        fAuth = FirebaseAuth.getInstance();
 
-        firstname=findViewById(R.id.editTextFirstName);
-        lastname=findViewById(R.id.editTextLastName);
-        Mobilenumber=findViewById(R.id.editTextPhone);
-        Address=findViewById(R.id.editTextaddr);
-        UserName=findViewById(R.id.editTextUserName);
-        password=findViewById(R.id.editTextPassword);
-        B.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String usrname=UserName.getText().toString().trim();
-                String pswd=password.getText().toString().trim();
-                if(TextUtils.isEmpty(usrname)){
-                    UserName.setError("Username is Required");
-                    return;
-                }
-                if(TextUtils.isEmpty(pswd)){
-                    password.setError("Password is Empty");
-                    return;
-                }
-                fAuth.createUserWithEmailAndPassword(usrname,pswd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(AddFaculty.this,"User Successfully added",Toast.LENGTH_LONG).show();
-                            Intent I = new Intent(getApplicationContext(), HomeScreenActivity.class);
-                            startActivity(I);
-                        }else{
-                            Toast.makeText(AddFaculty.this,"Error Occured"+task.getException().getMessage(),Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
+        db = FirebaseFirestore.getInstance();
 
-                //setContentView(R.layout.home_screen);
-            }
-        });
+        etFirstName = findViewById(R.id.editTextFirstName);
+        etLastName = findViewById(R.id.editTextLastName);
+        etUsername = findViewById(R.id.editTextUserName);
+        etPassword = findViewById(R.id.editTextPassword);
+        etAddress = findViewById(R.id.editTextaddr);
+        etPhone = findViewById(R.id.editTextPhone);
     }
 
 
@@ -77,4 +45,102 @@ public class AddFaculty extends AppCompatActivity {
         startActivity(I);
         //setContentView(R.layout.home_screen);
     }
+
+    public void addFacultyClick(View view) {
+        String firstName = etFirstName.getText().toString();
+        String lastName = etLastName.getText().toString();
+        String username = etUsername.getText().toString();
+        String address = etAddress.getText().toString();
+        String password = etPassword.getText().toString();
+        String phone = etPhone.getText().toString();
+
+        if (!validateInputs(firstName, lastName, username, address, password, phone)) return;
+
+        Faculty faculty = new Faculty(firstName, lastName, username, address, password, phone);
+
+        verifyUsernameUnique(faculty);
+    }
+
+    private void verifyUsernameUnique(final Faculty faculty) {
+        db.collection("faculty")
+                .whereEqualTo(Constants.username,faculty.getUsername())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            if (task.getResult().getDocuments().size()>0){
+                                Toast.makeText(AddFaculty.this, getString(R.string.username_already_exist), Toast.LENGTH_SHORT).show();
+                            }else{
+                                addFacultyToDatabase(faculty);
+                            }
+                        }else{
+                            Toast.makeText(AddFaculty.this, getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    private void addFacultyToDatabase(Faculty faculty) {
+        db.collection("faculty")
+                .add(faculty)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(AddFaculty.this, "Faculty Added!", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(AddFaculty.this, getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private boolean validateInputs(String firstName, String lastName, String username, String address, String password, String phone) {
+
+        if (firstName.isEmpty()) {
+            Toast.makeText(this, "First Name cannot be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (lastName.isEmpty()) {
+            Toast.makeText(this, "Last Name cannot be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (phone.isEmpty()) {
+            Toast.makeText(this, "Phone cannot be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (address.isEmpty()) {
+            Toast.makeText(this, "Address cannot be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (username.isEmpty()) {
+            Toast.makeText(this, "Username cannot be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (password.isEmpty()) {
+            Toast.makeText(this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (password.length() < 4 || password.length() >= 20) {
+            Toast.makeText(this, "Password must be at least 4 characters", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+
+    }
+
 }
+
--- app/src/main/java/com/example/attendancemanager/AddStudentActivity.java
+++ app/src/main/java/com/example/attendancemanager/AddStudentActivity.java
@@ -3,33 +3,145 @@ package com.example.attendancemanager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.attendancemanager.models.Student;
+import com.example.attendancemanager.other.Constants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+//import com.example.attendancemanager.models.Student;
+
 public class AddStudentActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+    private EditText etFirstName;
+    private EditText etLastName;
+    private Spinner spinnerDept;
+    private Spinner spinnerYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_student);
 
+        db = FirebaseFirestore.getInstance();
+
+        etFirstName = findViewById(R.id.editTextFirstName);
+        etLastName = findViewById(R.id.editTextLastName);
+        spinnerDept = findViewById(R.id.spinnerdept);
+        spinnerYear = findViewById(R.id.spinneryear);
+
     }
 
     public void submit(View v) {
-        Intent I = new Intent(getApplicationContext(), HomeScreenActivity.class);
-        startActivity(I);
-        //setContentView(R.layout.home_screen);
-        Toast.makeText(getApplicationContext(), "Student Successfully Added", Toast.LENGTH_LONG).show();
+        String  firstName = etFirstName.getText().toString();
+        String  lastName = etFirstName.getText().toString();
+
+        if (!validateInputs(firstName,lastName)) return;
+        fetchRecentStudentId(firstName,lastName,spinnerDept.getSelectedItem().toString(),spinnerYear.getSelectedItem().toString());
     }
 
     public void cancel(View v) {
-        Intent I = new Intent(getApplicationContext(), HomeScreenActivity.class);
-        startActivity(I);
-       // setContentView(R.layout.home_screen);
+        finish();
     }
 
+    private boolean validateInputs(String firstName,String lastName){
 
+        if (firstName.isEmpty()){
+            Toast.makeText(this, getString(R.string.first_name_cannot_empty), Toast.LENGTH_SHORT).show();
+            return false;
+        }
 
+        if (lastName.isEmpty()){
+            Toast.makeText(this, getString(R.string.last_name_cannot_empty), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void fetchRecentStudentId(final String firstName, final String lastName, final String dept, final String year){
+
+        db.collection(Constants.id)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            int id = Integer.parseInt(task.getResult().getDocuments().get(0).getData().get(Constants.recentStudentId).toString());
+                            Map<String,Boolean> map = new HashMap<>();
+                            Student student = new Student(
+                                    id,
+                                    new Timestamp(new Date()),
+                                    firstName,
+                                    lastName,
+                                    year,
+                                    dept,
+                                    0,
+                                    0,
+                                    map,
+                                    map,
+                                    map,
+                                    map,
+                                    map
+                            );
+                            addStudent(student);
+                        }else{
+                            Toast.makeText(AddStudentActivity.this, getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    private void addStudent(Student student){
+
+        db.collection(Constants.student)
+                .add(student)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        incrementRecentStudentId();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(AddStudentActivity.this, getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void incrementRecentStudentId(){
+        db.collection(Constants.id).document(Constants.recentStudentIndex)
+                .update(Constants.recentStudentId, FieldValue.increment(1)).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(AddStudentActivity.this, getString(R.string.student_added), Toast.LENGTH_SHORT).show();
+                    finish();
+                }else{
+                    Toast.makeText(AddStudentActivity.this, getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 
 }
